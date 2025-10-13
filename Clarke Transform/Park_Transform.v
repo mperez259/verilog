3177dc6a1459b4af9aa74d2cf353578b67e2a978
m. perez <bm3prz@gmail.com>
2025-10-12 20:57:47 -0700

    Add files via upload

---
+++ Clarke Transform/Park_Transform.v
@@ -0,0 +1,11 @@
+`timescale 1ns / 1ps
+
+module Park_Transform(
+    input signed [15:0] i_alpha, i_beta,
+    input signed [15:0] sin_theta, cos_theta,
+    output reg signed [15:0] i_d, i_q
+    );
+    
+    
+    
+endmodule
